c402c725e61102da06119e8ecc22e45b75a0742c
wsChoe <choe_9007@naver.com>
2024-03-18 15:08:35 +0900

    add

--- tranning_Mar24/src/tranning_Mar24/tranning001.java
+++ tranning_Mar24/src/tranning_Mar24/tranning001.java
@@ -4,7 +4,12 @@ public class tranning001 {
 
 	public static void main(String[] args) {
 
-		String test = "OO2";
+		String[] text = new String[3];
+		text[0] = "Hello";
+		text[1] = "Java";
+		text[2] = "Program";
+		System.out.print(text[0], text[1], text[2]);
+
 	}
 
 }
